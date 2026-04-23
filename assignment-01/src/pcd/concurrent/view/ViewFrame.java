5e1b0c27a85b8fa9b114662189eab0bbaad79c47
Elisa Yan <elisa.yan@studio.unibo.it>
2026-04-23 19:03:36 +0200

    paint hole

--- assignment-01/src/pcd/concurrent/view/ViewFrame.java
+++ assignment-01/src/pcd/concurrent/view/ViewFrame.java
@@ -1,99 +1,114 @@
 package pcd.concurrent.view;
 
+import pcd.concurrent.model.Board;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ViewFrame extends JFrame {
-    
+
     private VisualiserPanel panel;
     private ViewModel model;
     private RenderSynch sync;
-    
-    public ViewFrame(ViewModel model, int w, int h){
-    	this.model = model;
-    	this.sync = new RenderSynch();
-    	setTitle("Poool Game - multithreaded version");
-        setSize(w,h + 25);
+
+    public ViewFrame(ViewModel model, int w, int h) {
+        this.model = model;
+        this.sync = new RenderSynch();
+        setTitle("Poool Game - multithreaded version");
+        setSize(w, h + 25);
         setResizable(false);
-        panel = new VisualiserPanel(w,h);
+        panel = new VisualiserPanel(w, h);
         getContentPane().add(panel);
-        addWindowListener(new WindowAdapter(){
-			public void windowClosing(WindowEvent ev){
-				System.exit(-1);
-			}
-			public void windowClosed(WindowEvent ev){
-				System.exit(-1);
-			}
-		});
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent ev) {
+                System.exit(-1);
+            }
+
+            public void windowClosed(WindowEvent ev) {
+                System.exit(-1);
+            }
+        });
     }
-     
-    public void render(){
-		long nf = sync.nextFrameToRender();
+
+    public void render() {
+        long nf = sync.nextFrameToRender();
         panel.repaint();
-		try {
-			sync.waitForFrameRendered(nf);
-		} catch (InterruptedException ex) {
-			ex.printStackTrace();
-		}
+        try {
+            sync.waitForFrameRendered(nf);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
     }
-        
+
     public class VisualiserPanel extends JPanel {
         private int ox;
         private int oy;
         private int delta;
-        
-        public VisualiserPanel(int w, int h){
-            setSize(w,h + 25);
-            ox = w/2;
-            oy = h/2;
+
+        public VisualiserPanel(int w, int h) {
+            setSize(w, h + 25);
+            ox = w / 2;
+            oy = h / 2;
             delta = Math.min(ox, oy);
         }
 
-        public void paint(Graphics g){
-    		Graphics2D g2 = (Graphics2D) g;
-    		
-    		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-    		          RenderingHints.VALUE_ANTIALIAS_ON);
-    		g2.setRenderingHint(RenderingHints.KEY_RENDERING,
-    		          RenderingHints.VALUE_RENDER_QUALITY);
-    		g2.clearRect(0,0,this.getWidth(),this.getHeight());
-            
-    		g2.setColor(Color.LIGHT_GRAY);
-		    g2.setStroke(new BasicStroke(1));
-    		g2.drawLine(ox,0,ox,oy*2);
-    		g2.drawLine(0,oy,ox*2,oy);
-    		g2.setColor(Color.BLACK);
-    		
-    		    g2.setStroke(new BasicStroke(1));
-	    		for (var b: model.getBalls()) {
-	    			var p = b.pos();
-	            	int x0 = (int)(ox + p.x()*delta);
-	                int y0 = (int)(oy - p.y()*delta);
-	                int radiusX = (int)(b.radius()*delta);
-	                int radiusY = (int)(b.radius()*delta);
-	                g2.drawOval(x0 - radiusX,y0 - radiusY,radiusX*2,radiusY*2);
-	    		}
-	
-    		    g2.setStroke(new BasicStroke(3));
-	    		var pb = model.getPlayerBall();
-	    		if (pb != null) {
-					var p1 = pb.pos();
-		        	int x0 = (int)(ox + p1.x()*delta);
-		            int y0 = (int)(oy - p1.y()*delta);
-	                int radiusX = (int)(pb.radius()*delta);
-	                int radiusY = (int)(pb.radius()*delta);
-	                g2.drawOval(x0 - radiusX,y0 - radiusY,radiusX*2,radiusY*2);
-	    		}
-    		    
-    		    g2.setStroke(new BasicStroke(1));
-	    		g2.drawString("Num small balls: " + model.getBalls().size(), 20, 40);
-	    		g2.drawString("Frame per sec: " + model.getFramePerSec(), 20, 60);
+        public void paint(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
 
-	    		sync.notifyFrameRendered();
-    		
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+                    RenderingHints.VALUE_RENDER_QUALITY);
+            g2.clearRect(0, 0, this.getWidth(), this.getHeight());
+
+            //holes
+            g2.setColor(Color.BLACK);
+            int holeRad = (int) (Board.HOLE_RADIUS * delta);
+            //left hole
+            int hlx = (int) (ox + Board.LEFT_HOLE_POS.x() * delta);
+            int hly = (int) (oy - Board.LEFT_HOLE_POS.y() * delta);
+            g2.fillOval(hlx - holeRad, hly - holeRad, holeRad * 2, holeRad * 2);
+            //right hole
+            int hrx = (int) (ox + Board.RIGHT_HOLE_POS.x() * delta);
+            int hry = (int) (oy - Board.RIGHT_HOLE_POS.y() * delta);
+            g2.fillOval(hrx - holeRad, hry - holeRad, holeRad * 2, holeRad * 2);
+
+            g2.setColor(Color.LIGHT_GRAY);
+            g2.setStroke(new BasicStroke(1));
+            g2.drawLine(ox, 0, ox, oy * 2);
+            g2.drawLine(0, oy, ox * 2, oy);
+            g2.setColor(Color.BLACK);
+
+            g2.setStroke(new BasicStroke(1));
+            for (var b : model.getBalls()) {
+                var p = b.pos();
+                int x0 = (int) (ox + p.x() * delta);
+                int y0 = (int) (oy - p.y() * delta);
+                int radiusX = (int) (b.radius() * delta);
+                int radiusY = (int) (b.radius() * delta);
+                g2.drawOval(x0 - radiusX, y0 - radiusY, radiusX * 2, radiusY * 2);
+            }
+
+            g2.setStroke(new BasicStroke(3));
+            var pb = model.getPlayerBall();
+            if (pb != null) {
+                var p1 = pb.pos();
+                int x0 = (int) (ox + p1.x() * delta);
+                int y0 = (int) (oy - p1.y() * delta);
+                int radiusX = (int) (pb.radius() * delta);
+                int radiusY = (int) (pb.radius() * delta);
+                g2.drawOval(x0 - radiusX, y0 - radiusY, radiusX * 2, radiusY * 2);
+            }
+
+            g2.setStroke(new BasicStroke(1));
+            g2.drawString("Num small balls: " + model.getBalls().size(), 20, 40);
+            g2.drawString("Frame per sec: " + model.getFramePerSec(), 20, 60);
+
+            sync.notifyFrameRendered();
+
         }
-        
+
     }
 }
